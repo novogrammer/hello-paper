ae183d63925ecbd519f50701ac7765cd79beebcf
Yusuke Kawamoto <novogrammer@gmail.com>
2024-06-12 19:18:16 +0900

    paper.js

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,30 @@
 import './style.css'
 
+import paper from "paper";
+
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    Hello!
-  </div>
+  <section class="p-section-first">
+    <canvas class="p-section-first__view"></canvas>
+  </section>
 `
 
+async function mainAsync(){
+  const viewElement=document.querySelector<HTMLCanvasElement>(".p-section-first__view");
+  if(!viewElement){
+    throw new Error("viewElement is null");
+  }
+  paper.setup(viewElement);
+  const path = new paper.Path();
+  path.strokeColor = new paper.Color('black');
+  const start = new paper.Point(100, 100);
+  path.moveTo(start);
+
+  path.lineTo(start.add([ 200, -50 ]));
+  // paper.view.draw();
+  path.
+}
+
+
+mainAsync().catch((error)=>{
+  console.error(error);
+})
